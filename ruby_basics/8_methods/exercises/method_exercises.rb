1e2f598830fe2772d661ac81edefdc26f25739b4
Maks2712 <maks2712@yahoo.com>
2022-08-13 12:04:04 +0700

    solved

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -2,36 +2,59 @@
 # To gain more familiarity, look up the documentation for each hint.
 # Remember to unskip the corresponding tests one at a time.
 
-# method name: #ascii_translator
-# parameter: number (an integer)
-# return value: the number's ASCII character (https://www.ascii-code.com/)
-# hint: use Integer#chr
+def ascii_translator(number)
+    # method name: #ascii_translator
+    # parameter: number (an integer)
+    # return value: the number's ASCII character (https://www.ascii-code.com/)
+    # hint: use Integer#chr
+    number.chr
+end
 
-
+def common_sports (current_sports, favorite_sports)
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+current_sports & favorite_sports
+end
 
-
+def alphabetical_list(games)
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
+games.sort.uniq
+end
 
-
+def lucky_number(number=7)
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+"Today's lucky number is #{number}"
+end
 
+def ascii_code(character)
+    # method name: #ascii_code
+    # parameter: character (a string)
+    # return value: the character's ordinal number
+    # explicit return value: 'Input Error' if character's length does not equal 1
+    # hint: use String#ord
+    if (character.length!=1)
+        return 'Input Error'
+    else
+        character.ord
+    end
+end
 
-# method name: #ascii_code
-# parameter: character (a string)
-# return value: the character's ordinal number
-# explicit return value: 'Input Error' if character's length does not equal 1
-# hint: use String#ord
-
-
+def pet_pun(animal)
+    case animal
+    when 'cat'
+        puts 'Cats are purr-fect!'
+    when 'dog'
+        puts 'Dogs are paw-some!'
+    else
+        puts "I think #{animal}s have pet-tential!"
+    end
 # method name: #pet_pun
 # parameter: animal (a string)
 # return value: nil
@@ -39,10 +62,12 @@
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
+end
 
-
+def twenty_first_century?(year)
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
-
+year.between?(2001,2100)
+end
